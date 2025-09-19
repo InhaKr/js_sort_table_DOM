9dbd0cfce7646b976e07d038cc3929eb247c680f
InhaKr <inkr3891@gmail.com>
2025-09-19 14:16:53 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const title = document.querySelectorAll('thead th');
+const titleArray = Array.from(title);
+
+const tdName = document.querySelectorAll('tbody tr');
+const tdArray = Array.from(tdName);
+
+const tbody = document.querySelector('tbody');
+
+titleArray.forEach((th) => {
+  th.addEventListener('click', (e) => {
+    const cellIndex = e.target.cellIndex;
+
+    // tdArray.forEach((tr) => {
+    //   console.log(tr.cells[cellIndex].textContent);
+    // });
+
+    tdArray.sort((rowA, rowB) => {
+      const valueA = rowA.cells[cellIndex].textContent;
+      const valueB = rowB.cells[cellIndex].textContent;
+      return valueA.localeCompare(valueB);
+    });
+
+    tbody.innerHTML = '';
+    tdArray.forEach((row) => tbody.appendChild(row));
+  });
+});
